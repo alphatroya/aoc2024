e3c3f1c75763f784798e182f243bb7275493c189
Alexey Korolev <alphatroya@gmail.com>
2024-12-03 11:53:57 +0300

    Join day1 a and b modules

--- day1/main.go
+++ day1/main.go
@@ -4,29 +4,58 @@ import (
 	_ "embed"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
 
 func main() {
+	mode := "a"
+	if len(os.Args) >= 2 {
+		mode = os.Args[1]
+	}
+
 	left, right, err := getInputs(rawInput)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "failed to build input, err=%v", err)
 		os.Exit(1)
 	}
 
-	result := 0
-	for _, l := range left {
-		counter := 0
-		for _, r := range right {
-			if r == l {
-				counter++
+	switch mode {
+	case "a":
+		sort.Slice(left, func(i, j int) bool {
+			return left[i] < left[j]
+		})
+
+		sort.Slice(right, func(i, j int) bool {
+			return right[i] < right[j]
+		})
+
+		result := 0
+		for i, l := range left {
+			distance := l - right[i]
+			if distance < 0 {
+				distance = -distance
 			}
+			result += int(distance)
 		}
-		result += int(l) * counter
+
+		fmt.Println(result)
+	case "b":
+		result := 0
+		for _, l := range left {
+			counter := 0
+			for _, r := range right {
+				if r == l {
+					counter++
+				}
+			}
+			result += int(l) * counter
+		}
+
+		fmt.Println(result)
 	}
 
-	fmt.Println(result)
 }
 
 type item int
